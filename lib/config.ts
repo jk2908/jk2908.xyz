79dea4ac9175a5c4d3f0e6a51b7e4bca8430fdcd
Jerome Kenway <jeromekenway@gmail.com>
2023-11-07 19:43:58 +0000

    Update config

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
 export const ME = 'Jerome Kenway'
-export const SITE = 'jk2908.xyz'
+export const URL = 'jk2908.xyz'
 export const DESCRIPTION = 'Frontend developer'
 export const EMAIL = 'jeromekenway@gmail.com'
