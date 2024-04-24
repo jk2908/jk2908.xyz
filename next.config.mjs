00487568a74ce9fac5118605fccd620682b6fc31
Jerome Kenway <jerome@digitalmarmalade.co.uk>
2024-04-24 21:16:45 +0100

    Add redirect to from old post URL

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,14 @@ const config = {
   reactStrictMode: true,
   experimental: {
     ppr: true,
-  }
+  },
+  redirects: async () => [
+    {
+      source: '/hey-future-colleague',
+      destination: '/recent-tinkering',
+      permanent: true,
+    },
+  ],
 }
 
 export default config
