05a92fc25da1ab96c410dd6c1c959c410781b4e8
Jerome Kenway <jerome@digitalmarmalade.co.uk>
2024-05-23 21:31:28 +0100

    Next 15

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,17 @@ const config = {
   reactStrictMode: true,
   experimental: {
     ppr: true,
-    reactCompiler: true
+    reactCompiler: true,
   },
-  redirects: async () => [
-    {
-      source: '/hey-future-colleague',
-      destination: '/recent-tinkering',
-      permanent: true,
-    },
-  ],
+  redirects() {
+    return [
+      {
+        source: '/hey-future-colleague',
+        destination: '/recent-tinkering',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default config
